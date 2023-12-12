e7b281870205adebb06cc57d93aa0461faaeef75
htmlars <125216721+htmlars@users.noreply.github.com>
2023-12-12 11:21:23 +0100

    Update delete_quote.c

--- delete_quote.c
+++ delete_quote.c
@@ -22,5 +22,21 @@ void delete_quote(struct Quote quotes[], int *numOfQuotes) {
 
     (*numOfQuotes)--;
     printf("Quote at index %d has been deleted.\n", index);
+
+    FILE *fp = fopen("D:/Semester_1_Projekte/Software/Project_Chicago/quotes.txt", "w");
+    if (fp == NULL) {
+        perror("Error opening file for writing!\n");
+        return;
+    }
+
+    for (int i = 0; i < *numOfQuotes; i++) {
+        if (i != index - 1) {
+            fprintf(fp, "\"%s\" (%s, %d) Source: %s | Page: %d\n",
+                    quotes[i].quote, quotes[i].author, quotes[i].date,
+                    quotes[i].source, quotes[i].page);
+        }
+    }
+
+    fclose(fp);
 }
 
